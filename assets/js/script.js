2c109f846f02151383aa53bc91763327a1917f10
LAPTOP-67TNSMIJ\Damas <online.dnwdms@gmail.com>
2021-02-24 12:27:15 +0700

    tambah asset

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function home() {
+    var elmnt = document.getElementById("home");
+    elmnt.scrollIntoView();
+  }
+function populer() {
+    var elmnt = document.getElementById("populer");
+    elmnt.scrollIntoView();
+  }
+
+function tentang() {
+    var elmnt = document.getElementById("tentang");
+    elmnt.scrollIntoView();
+  }
+function kontak() {
+    var elmnt = document.getElementById("kontak");
+    elmnt.scrollIntoView();
+  }
